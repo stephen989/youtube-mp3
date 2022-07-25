1be03956f3b5d5844ca81b349966e80c89f12a29
stephen989 <52377100+stephen989@users.noreply.github.com>
2022-07-26 00:13:37 +0100

    create file paths

--- music.py
+++ music.py
@@ -1,4 +1,7 @@
-class Song:
+import ffmpeg
+import youtube_dl
+
+class Track:
     def __init__(self,
                  title,
                  artist,
@@ -8,19 +11,53 @@ class Song:
         self.artist = artist
         self.album = album
         self.length = length
+        
     
     def create(dict_entry):
-        return Song(dict_entry["track_name"],
+        return Track(dict_entry["track_name"],
                    dict_entry['artist_name'],
                    dict_entry['album_name'])
+    
+    def create_path(self):
+        if not os.path.exists(os.path.join("Downloads", self.artist)):
+            os.mkdir(os.path.join("Downloads", self.artist))
+        if not os.path.exists(os.path.join("Downloads", self.artist, self.album)):
+            os.mkdir(os.path.join("Downloads", self.artist, self.album))
+        return os.path.join("Downloads", self.artist, self.album)
+    
+    def search(self):
+        query = " ".join([self.title, self.artist, self.album]) 
+        search = CustomSearch(query, VideoSortOrder.relevance, limit = 1)
+        url = search.result()['result'][0]['link']
+        return url
+    
+    
+    def download(self):
+        path = self.create_path()
+        url = self.search()
+        config = {
+            'verbose': True,
+            'outtmpl': f'downloads/{os.path.join(path, self.title)}.%(ext)s',
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '256',
+            }]}
+        
+        with youtube_dl.YoutubeDL(config) as ydl:
+            ydl.download([url])
+            title = ydl.extract_info(url, download = False)["title"]
+        print("Done")
+        
 
 
 class Artist:
-	def __init__(self,
-				 name):
-		self.name = name
+    def __init__(self,
+                 name):
+        self.name = name
 
 class Album:
-	def __init__(self, title, artist):
-		self.title = title
-		self.artist = artist
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
